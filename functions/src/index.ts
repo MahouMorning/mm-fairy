fa222d1877df88a869763b0ed9f896250919c9a6
Chase 李 <chase@mahoumorning.com>
2022-02-12 17:15:30 -0600

    Adding unit testing base files.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 
+type HttpsOnRequestHandler = Parameters<typeof functions.https.onRequest>[0]
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -7,3 +8,10 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+//
+export const addMessage = functions.https.onRequest((request, response) => {
+  console.log("This is a addmessage call.")
+  response.send("Hello from SendMessage.")
+})
+
+export const hello = () => 'Hello World!'
